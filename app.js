bdc0205c273270f384fd825024d45f832754cff9
elroistav <elroistav@gmail.com>
2024-12-09 20:01:02 +0200

    Using Express: 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/', (req, res) => {
-res.send('<html><body><h1>GET</h1></body></html>');
+    res.send('<html><body><h1>GET</h1></body></html>');
 });
 app.post('/', (req, res) => {
 res.send('<html><body><h1>POST</h1></body></html>');
